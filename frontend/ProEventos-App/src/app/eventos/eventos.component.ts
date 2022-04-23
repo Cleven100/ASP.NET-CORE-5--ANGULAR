81ecabc137ebfc0ab9d246503c195074fade5c0f
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-23 18:45:33 -0300

    23/04

--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,26 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any = [
+  public eventos: any;
 
-
-    {
-      Tema: 'Angular 11',
-      Local: 'Belo Horizonte'
-     },
-     {
-      Tema: '.net 5',
-      Local: 'São paulo'
-     },
-     {
-      Tema: 'Angular e suas novidades',
-      Local: 'Rio de janeiro'
-     }
-]
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+
+    this.http.get(`https://localhost:5001/api/eventos`).subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+
+
+  
   }
 
 }
