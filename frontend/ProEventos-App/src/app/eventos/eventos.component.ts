8b0f36ee4420cfe02e1e4f89a314c453791a3f82
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-30 15:57:26 -0300

    30/04

--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
@@ -9,7 +10,26 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
-  filtroLista: string = '';
+  public eventosFiltrados: any = [];
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista
+  }
+
+ public set filtroLista(value: string) {
+   this._filtroLista = value;
+
+   this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+
+ }
+
+ filtrarEventos(filtrarPor: string) : any{
+   filtrarPor = filtrarPor.toLocaleLowerCase();
+   return this.eventos.filter(
+     ( evento: { tema: string; local: string}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+   )
+ }
 
   constructor(private http: HttpClient) { }
 
@@ -20,7 +40,10 @@ export class EventosComponent implements OnInit {
   public getEventos(): void {
 
     this.http.get(`https://localhost:5001/api/eventos`).subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
 
